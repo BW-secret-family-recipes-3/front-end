dda29bd9aef5775b44075b9d610c86f8ef8e38da
William Herman <williamajherman@gmail.com>
2020-09-22 08:13:54 -0400

    created NavBar component

---
+++ secret-family-recipes/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from "react-redux";
+
+function Navbar(props){
+    return(
+        <div>
+            <h2>Navbar</h2>
+            {/* Navbar component goes here*/}
+        </div>
+    );
+};
+
+export default Navbar;
