ce0f7c3e84bb184250074dd0472ad29ce3f6f903
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-09-22 14:45:04 -0500

    added to viewRecipe component and fixed login password validation

--- secret-family-recipes/src/components/ViewRecipes.js
+++ secret-family-recipes/src/components/ViewRecipes.js
@@ -1,13 +1,49 @@
 // third party imports
 import React, {useEffect} from 'react';
 import { connect } from "react-redux";
+import {Link, Route, useRouteMatch} from 'react-router-dom';
+import styled from 'styled-components';
 
 // local imports
 import SearchRecipes from './SearchRecipes';
 import Recipe from './Recipe';
 // will move to login component 
 import { fetchTokenAction } from '../actions/fetchToken';
-import { registerUserAction } from '../actions/registerUser'
+import { registerUserAction } from '../actions/registerUser';
+
+
+
+// DUMMY DATA!
+const exampleRecipes = [{
+    title: "example recipe Apple Pie",
+    source: "Parker",
+    ingredients: ["brown sugar", "apples", "lard", "flour", "secret spices"],
+    instructions: "Mix flour, lard, and sugar into dough. Make apple filling with secret spices. Bake.",
+    categories: ["dessert", "fruit", "decadent"]
+},
+{
+    title: "example recipe Potato Latkes",
+    source: "Grandma Helen",
+    ingredients: ["russet potatos", "sour cream", "neutral oil", "chives", "egg whites"],
+    instructions: "Grate potatoes, add egg whites, from into latkes, and shallow fry. Serve with applesauce and sour cream.",
+    categories: ["holidays", "channukah", "thanksgiving", "fried"]
+},
+{
+    title: 'example recipe number three',
+    source: 'Gordon Ramsay',
+    ingredients: ['this', 'that', 'wow', 'why?', 'eggs'],
+    instructions: 'First you do this, then you do that, then you cook it and eat it and wow',
+    categories: ['everyday', 'tomorrow', 'today', 'huh?', 'now']
+},
+{
+    title: 'Pizza',
+    source: 'Papa John',
+    ingredients: ['Dough', 'Cheese', 'Sauce', 'Pepperoni'],
+    instructions: 'First you make the dough, then put the sauce on it. Put the cheese and pepperoni on top and throw it in the oven until it\'s done',
+    categories: ['everyday', 'breakfast', 'lunch', 'dinner']
+}];
+
+
 
 function ViewRecipes(props){
 
@@ -33,6 +69,13 @@ function ViewRecipes(props){
     return (
         <div>
             <h2>View Recipes</h2>
+            <div>
+                {exampleRecipes.map(rec=>{
+                    return (
+                        <Recipe recipe={rec}/>
+                    )
+                })}
+            </div>
             <SearchRecipes/>
             {/* ViewRecipes component goes here*/}
             <div className = "recipesContainer">
