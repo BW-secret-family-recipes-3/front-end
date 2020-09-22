4cee208ffc4490930a451cd2ef00c814cff03830
William Herman <williamajherman@gmail.com>
2020-09-22 08:22:29 -0400

    added loggedIn and loggedOut links, now need to add ternary and connect props

--- secret-family-recipes/src/components/NavBar.js
+++ secret-family-recipes/src/components/NavBar.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import {BrowserRouter as Router, Link} from "react-router-dom";
 import { connect } from "react-redux";
+
+const loggedInLinks = () => {
+    return (
+        <div>
+            <Link to = "/user/dashboard">Dashboard</Link>
+        </div>
+    );
+};
+
+const loggedOutLinks = () => {
+    return (
+        <div>
+            <Link to = "/user/login">Login</Link>
+            <Link to = "/user/register">Register</Link>
+        </div>
+    );   
+};
 
 function NavBar(props){
     return(
         <div>
-            <h2>NavBar</h2>
-            {/* Navbar component goes here*/}
+           {loggedOutLinks()}
         </div>
     );
 };
